aa29526c306b4d8c2fafe77149918c6afde68c08
Reda <redabaddy2@email.com>
2026-04-24 23:25:05 +0100

    removed packages sentence-transformers, faster-whisper

--- vectordb_generation.py
+++
@@ -1,9 +0,0 @@
-from src.services.rag import RAGService
-ingest_files = RAGService().ingest_files
-
-file_paths = [
-    "data/CPI EMINES.pdf",
-    "data/Brochure UM6P.pdf",
-    "data/cleaned_emines_docs.json"
-]
-ingest_files(file_paths)
